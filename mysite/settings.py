62e9bf547871ddd1bbd77692a97dfa54d725e4f9
Saphereye <adarshdas950@gmail.com>
2023-04-02 12:10:01 +0530

    Protected key

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-mrsn4))*eoj42_w!jn$@y+5@_a@*80oyf_jg$o5@rpl8to8!*c'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ['SECRET_KEY']
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
